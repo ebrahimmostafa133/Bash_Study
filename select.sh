8fc36c49380e56ef23825d79d4d9bbd145d4f4ab
ebrahimmostafa133 <ibrahim.mostafa9939@gmail.com>
2026-03-14 03:52:12 +0200

    add select file

---
+++ select.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# --- Bash 'select' Loop (Interactive Menus) ---
+
+# The 'select' command provides an easy way to create numbered menus.
+# It automatically prints a numbered list of the items, prompts the user
+# with the PS3 variable, and reads their input.
+
+# 1. Customizing the prompt
+# By default, the prompt is "#? ". We can change it by modifying 'PS3'.
+PS3="Please select an option (1-4): "
+
+echo "=== Simple Menu Example ==="
+# 2. Defining the menu options
+options=("Option 1" "Option 2" "Option 3" "Quit")
+
+# 3. The select loop
+select opt in "${options[@]}"; do
+    case $opt in
+        "Option 1")
+            echo "You chose Option 1. This could run a specific command."
+            ;;
+        "Option 2")
+            echo "You chose Option 2."
+            ;;
+        "Option 3")
+            echo "You chose Option 3."
+            ;;
+        "Quit")
+            echo "Exiting the menu."
+            break # Essential: This breaks out of the loop!
+            ;;
+        *)
+            # If the user enters a number that doesn't correspond to an option,
+            # $opt will be empty. The raw user input is stored in the $REPLY variable.
+            echo "Invalid option: $REPLY. Please enter a number between 1 and ${#options[@]}."
+            ;;
+    esac
+    # After the case statement, the select loop will automatically re-prompt
+    # unless a 'break' command was executed.
+done
+
+echo "Menu finished."
